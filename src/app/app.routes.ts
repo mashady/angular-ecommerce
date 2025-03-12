5d5a95004f31aca50c81d0fd3d0233ec17997078
mashady <muhammedmashady@gmail.com>
2025-03-12 16:44:56 +0200

    adding project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home Page',
+  },
+  {
+    path: 'products-list',
+    component: ProductListComponent,
+    title: 'Poroducts List Page',
+  },
+  {
+    path: 'product-details/:id',
+    component: ProductDetailsComponent,
+    title: 'Product Details Page',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login Page',
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Register Page',
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    title: 'Not Found Page',
+  },
+];
