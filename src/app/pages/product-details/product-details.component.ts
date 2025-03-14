4ac10f2726417d0a114e6ce2e369bd68918ca405
Abdelwahab <abdelwahabsaeed415@gmail.com>
2025-03-14 02:11:15 +0200

    make the product interface

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-product-details',
   imports: [],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css'
+  styleUrl: './product-details.component.css',
 })
 export class ProductDetailsComponent {
-
+  product: Product = {
+    _id: '',
+    name: '',
+    description: '',
+    price: 100,
+    discount: 35,
+    category: '',
+    addedBy: '',
+    updatedBy: '',
+    images: [''],
+    stock: 20,
+    reviews: [''],
+    createdAt: '',
+    updatedAt: '',
+  };
 }
