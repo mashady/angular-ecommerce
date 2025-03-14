6ea521c1f7eb6cf937e58b26e150e90d3d262a50
hp <omarokstar2001@gmail.com>
2025-03-15 01:18:28 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { CartComponent } from './pages/cart/cart.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
