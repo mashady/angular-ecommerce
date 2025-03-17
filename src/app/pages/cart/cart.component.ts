6121b37dcb4e337b777537ecd2204f7d6b085d75
hp <omarokstar2001@gmail.com>
2025-03-17 22:39:22 +0200

    updates

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component ,inject} from '@angular/core';
+import { CartService } from '../../cart.service';
+import { CommonModule } from '@angular/common'; 
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule,HttpClientModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
+  myCart: any = { products: [] };
+  cartService=inject(CartService);
 
+  
+ngOnInit() {
+  
+this.loadCart();
+}
+
+loadCart(){
+  this.cartService.getUserCart().subscribe((data:any) => {
+    console.log('Cart Data:', data);
+    this.myCart = data || {products :[]};
+
+
+    });
+
+}
 }
